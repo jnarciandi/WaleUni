e86a889ab3a3f0aebc9a455527200fae4e3612b4
mati <mati@DESKTOP-90KPIC7>
2019-09-04 14:13:46 +0200

    rr

--- UniWales/src/Main.java
+++ UniWales/src/Main.java
@@ -7,6 +7,7 @@ public class Main {
 		Person p1 = new Student("pepe",80.2,1.87,1980,1,21,"Calle Villarroel 157");
 		System.out.println(p1.getAge());
 		System.out.println(p1.getBirth());
+		System.out.println(((Student) p1).getApartment().getAdress());
 
 	}
 
