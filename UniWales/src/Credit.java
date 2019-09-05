2adf9ac1e75e6c724d14941fb5c03412614a79f8
mati <mati@DESKTOP-90KPIC7>
2019-09-05 14:02:38 +0200

    actu credit

--- UniWales/src/Credit.java
+++ UniWales/src/Credit.java
@@ -1,4 +1,70 @@
 
+
+// CLASE SOLO MALCOPIADA!!!!
 public class Credit extends Card {
+	private double balance;
+    //Added debt atribute and its basic methods:
+    private double debt;
+ 
+    public double getDebt() {
+		return debt;
+	}
+
+	public void setDebt(double debt) {
+		this.debt = debt;
+	}
+
+	public Account(String owner, double balance) {
+        this.balance = balance;
+        this.owner = owner;
+        this.debt = 0.0;
+    }
+ 
+    public void deposit(double amount) {
+        balance += amount;
+    }
+    
+    public void debtIncrease(double amount) {
+    	double interest = (amount * 5.0) / 100.0;
+    	debt += amount +  interest;
+    }
+    
+    public void payDebt(double amount) {
+    	debt -= amount;
+    }
+ 
+    public void withdrawal(double amount) {
+        balance -= amount;
+    }
+ 
+    public double getBalance() {
+        return balance;
+    }
+    
+    public boolean confirmFunds(double amount) {
+    	if (this.balance < amount) {
+    		return false;
+    	}else {
+    		return true;
+    	}
+    }
+ 
+    public String getOwner() {
+		return owner;
+	}
+
+    public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
+    public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	@Override
+	public String toString() {
+		return "Account [balance=" + balance + ", owner=" + owner + ", debt=" + debt + "]";
+	}
+}
 
 }
